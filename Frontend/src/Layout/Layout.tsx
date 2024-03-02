d12d31d9bea602ae2e1deb0e17c814f98af4f62d
Jannie Hamberg <jg311209@live.com>
2024-03-02 13:51:40 +0100

    Fixed pages, components, router,layout

---
+++ Frontend/src/Layout/Layout.tsx
@@ -0,0 +1,13 @@
+import { Footer } from "../Components/Footer"
+import { Navbar } from "../Components/Navbar"
+import { Outlet } from "react-router-dom"
+
+export const Layout = () => {
+    return (
+        <div>
+            <Navbar />
+            <Outlet />
+            <Footer />
+        </div>
+    )
+}
---
+++ Frontend/src/Pages/Library.tsx
@@ -0,0 +1,7 @@
+
+
+export const Library = () => { 
+    return (
+        <>Library</>
+    )
+}

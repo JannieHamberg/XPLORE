ae55145709833a32951148e9191699fa09e4eaa0
Jannie Hamberg <jg311209@live.com>
2024-03-02 13:56:28 +0100

    fixed

--- Frontend/src/Router/Router.tsx
+++ Frontend/src/Router/Router.tsx
@@ -1,9 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
 import { Layout } from "../Layout/Layout";
 import { ErrorPage } from "../Pages/ErrorPage";
-import { Home } from "../Pages/Home";
 import { Search } from "../Pages/Search";
 import { Library } from "../Pages/Library";
+import { Home } from "../Pages/home";
 
 export const router = createBrowserRouter([
     {

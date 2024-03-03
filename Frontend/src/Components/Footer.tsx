f5408b55ef5302a79dd10752c4ecc9337b6d5b20
Jannie Hamberg <jg311209@live.com>
2024-03-03 21:51:54 +0100

    Footer almost done

--- Frontend/src/Components/Footer.tsx
+++ Frontend/src/Components/Footer.tsx
@@ -1,5 +1,35 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faFacebook, faInstagram, faSquareXTwitter } from '@fortawesome/free-brands-svg-icons';
+import { LogoutButton } from "./LogOut"
+
 export const Footer = () => { 
     return (
-        <>Footer</>
+        <div className="flex items-center justify-center mt-40 bg-custom-purple ">
+            <div className=" w-1/3 pl-20 text-white text-center">
+            <FontAwesomeIcon icon={faFacebook} size="2xl" className="px-4"/>
+            <FontAwesomeIcon icon={faInstagram} size="2xl" />
+            <FontAwesomeIcon icon={faSquareXTwitter} size="2xl" className="px-4"/>
+            </div>
+            <div className=" w-1/3 text-center text-white">
+                <ul>
+                    <li>
+                        <a href="#">Contact</a>
+                    </li>
+                    <li>
+                        <a href="#">Contributors</a>
+                    </li>
+                    <li>
+                        <a href="#">License</a>
+                    </li>
+                    <li className="text-purple-600">
+                        <LogoutButton />
+                    </li>
+                </ul>
+                
+                </div>
+            <div className=" w-1/3 text-right pr-20">
+                <img src="../../Images/xploreLoggaPurpule.png" alt="logo" />
+            </div>
+        </div>
     )
 }

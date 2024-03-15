78654f705e5752530c1c072044dd61136ccefccf
Jannie Hamberg <jg311209@live.com>
2024-03-15 12:40:17 +0100

    Fixed logic on server, and bug on client side.

--- Frontend/src/Models/FavoritesContext.tsx
+++ Frontend/src/Models/FavoritesContext.tsx
@@ -1,11 +1,11 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { createContext } from "react";
 import { IImage } from "./IImage";
-
-
+import { useAuth0 } from "@auth0/auth0-react";
+import axios from "axios";
 
 interface FavoritesContextType {
-    favorites: IImage[];
+    favorites: string[];
     addFavorite: (image: IImage) => void;
 }
 
@@ -20,14 +20,34 @@ interface FavoritesProviderProps {
 }
 
 export const FavoritesProvider: React.FC<FavoritesProviderProps> = ({ children }) => {
-    const [favorites, setFavorites] = useState<IImage[]>([]);
+    const [favorites, setFavorites] = useState<string[]>([]);
     const [showToast, setShowToast] = useState<boolean>(false);
+    const { user } = useAuth0();
 
-    const addFavorite = (image: IImage) => {
-        setFavorites((prevFavorites) => [...prevFavorites, image]);
-        setShowToast(true);
-        setTimeout(() => setShowToast(false), 6000);
-    };
+    const addFavorite = async (image: IImage) => {
+        if (user?.sub) {
+          try {
+            const response = await axios.post('http://localhost:3000/api/images/save', {
+              userId: user.sub,
+              favorites: [image.link], 
+            });
+            setShowToast(true);
+            setTimeout(() => setShowToast(false), 6000);
+            console.log(response.data.message);
+          } catch (error) {
+            console.error('Error saving favorite image', error);
+          }
+        }
+      };
+      
+      useEffect(() => {
+        if (user?.sub) {
+          axios.get(`http://localhost:3000/api/images/getfavorites/${user.sub}`)
+            .then(response => setFavorites(response.data))
+            .catch(error => console.log(error));
+        }
+      }, [user?.sub]);
+      
 
     return (
         
